29abc6a9d6f4b1ff7ee480d29e34badc715d795d
PN-Alumno <PN-Alumno@duoc.acad>
2026-03-17 16:40:45 -0300

    Spring Naruto

---
+++ obito/obito/src/main/java/com/example/naruto/obito/controller/obitoUchiha.java
@@ -0,0 +1,15 @@
+package com.example.naruto.obito.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class obitoUchiha {
+
+    @GetMapping("/Obito")
+    public String holaMundo(){
+        return"Obito fue el mejor personaje de naruto";
+    }
+    
+}
